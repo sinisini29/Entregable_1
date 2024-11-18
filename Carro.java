78a4c827587c209aba7c18ab43be8fa99ea747d8
Brayan Estiven Sinisterra Cabrera <166745720+sinisini29@users.noreply.github.com>
2024-11-18 13:37:21 -0500

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -2,6 +2,7 @@ public  class Carro extends Vehiculo{
     public Carro(String marca, String modelo, double precio){
         super(marca, modelo, precio);
     }
+    @override
     public String getTipo(){
         return "Carro";
     }

6a69a5cde03d80f8bd3f8bc3776ab94c8ee4d666
Brayan Estiven Sinisterra Cabrera <166745720+sinisini29@users.noreply.github.com>
2024-11-18 13:37:52 -0500

    Update Moto.java

--- Moto.java
+++ Moto.java
@@ -2,10 +2,8 @@ public class Moto extends Carro {
     public Moto(String marca, String modelo, double precio){
         super(marca, modelo, precio);
     }
-
     @Override
     public String getTipo(){
         return "Moto";
     }
-
 }
